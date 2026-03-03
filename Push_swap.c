ce16ab5061e816b9909756dfca45b5eeeccad1d5
GuidoMagni <guibonmagni@gmail.com>
2026-03-03 15:02:30 +0100

    changes

--- Push_swap.c
+++ Push_swap.c
@@ -6,29 +6,30 @@
 /*   By: gumagni <gumagni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 19:33:24 by guido             #+#    #+#             */
-/*   Updated: 2026/02/24 19:29:29 by gumagni          ###   ########.fr       */
+/*   Updated: 2026/03/03 15:01:36 by gumagni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Push_swap.h"
 
-void	ft_pushswap(int ac, char **av)
+void	ft_pushswap(t_list *stacka)
 {
-	
+	while (stacka)
+	{
+		printf("%d ", stacka->n);//dovro usare ft_printf mi sa
+		stacka = stacka->next;
+	}
+	printf("\n");
 }
-
-
-
-
-
 
 int	main(int ac, char **av)
 {
 	int	a;
-	t_list *stacka;
+	t_list	*stacka;
 
 	a = 1;
 	stacka = NULL;
 	while (a < ac)
 		ft_lstadd_back(&stacka, ft_lstnew(atoi(av[a++])));
+	ft_pushswap(stacka);
 }
